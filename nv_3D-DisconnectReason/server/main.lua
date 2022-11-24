db876e494cf20b569ab0e7c726faa67a9f6c7696
wobozkyng <42249669+wobozkyng@users.noreply.github.com>
2022-11-25 03:57:57 +0700

    collect player coord just on leave, removed loop

--- nv_3D-DisconnectReason/server/main.lua
+++ nv_3D-DisconnectReason/server/main.lua
@@ -1,23 +1,10 @@
-local P_TABLE = {}
+AddEventHandler('playerDropped', function(reason)
+    local source = source
+    local sourcePed = GetPlayerPed(source)
+    local pedCoords = GetEntityCoords(sourcePed)
+    local playerName = GetPlayerName(source)
 
-Citizen.CreateThread(function()
-    while true do
-        for k, v in pairs(GetPlayers()) do
-            local source = tonumber(v)
-            local coords = GetEntityCoords(GetPlayerPed(source))
-            if coords ~= nil then
-                P_TABLE[source] = GetEntityCoords(GetPlayerPed(source))
-            end
-        end
-        Citizen.Wait(3000)
-    end
-end)
-
-RegisterNetEvent('playerDropped')
-AddEventHandler('playerDropped', function (reason)
-    if P_TABLE[source] then
-        TriggerClientEvent('nv_Disconnect:sendPlayerDropped', -1, { src = source, name = GetPlayerName(source), razon = reason, coords = P_TABLE[source]})
-    end
+    TriggerClientEvent('nv_Disconnect:sendPlayerDropped', -1, { src = source, name = playerName, razon = reason, coords = pedCoords})
 end)
 
 --[[
